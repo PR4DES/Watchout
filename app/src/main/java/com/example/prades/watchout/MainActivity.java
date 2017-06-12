e4bd3938018ed9a971fc24cc067db0b4aaa9498b
SongYi Lee <qwe52165@gmail.com>
2017-06-13 01:26:12 +0900

    service + camera2

--- app/src/main/java/com/example/prades/watchout/MainActivity.java
+++ app/src/main/java/com/example/prades/watchout/MainActivity.java
@@ -1,15 +1,23 @@
 package com.example.prades.watchout;
 
+import android.Manifest;
+import android.app.ActivityManager;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
     Button button;
+    private static final int REQUEST_CAMERA_RESULT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,22 +25,52 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
 
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_RESULT);
+        }
+
         button = (Button)findViewById(R.id.button);
+        if(isMyServiceRunning(MainService.class)) {
+            button.setText("END");
+        }
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(button.getText().toString() == "END") {
+                    Intent intent = new Intent(MainActivity.this,MainService.class);
+                    stopService(intent);
                     button.setText("START");
-                    Intent intent = new Intent(MainActivity.this,MainService.class); // 이동할 컴포넌트
-                    stopService(intent); // 서비스 종료
                 }
                 else {
-                    button.setText("END");
                     Intent intent = new Intent(MainActivity.this,MainService.class);
                     startService(intent);
+                    button.setText("END");
                 }
             }
         });
+    }
+
+    private boolean isMyServiceRunning(Class<?> serviceClass) {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode){
+            case  REQUEST_CAMERA_RESULT:
+                if (grantResults[0] != PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(this, "Cannot run application because camera service permission have not been granted", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/prades/watchout/MainService.java
+++ app/src/main/java/com/example/prades/watchout/MainService.java
@@ -1,47 +1,210 @@
 package com.example.prades.watchout;
 
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.ImageFormat;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.Image;
+import android.media.ImageReader;
+import android.os.Build;
 import android.os.IBinder;
+import android.os.StrictMode;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.widget.Toast;
+
+import java.nio.Buffer;
+import java.util.Arrays;
 
 /**
  * Created by prades on 2017-06-08.
  */
 
 public class MainService extends Service {
-    private static final String TAG = "HelloService";
+    private static final String TAG = "MainService";
+    protected static int camchoice;
+
     int count = 0;
     private boolean isRunning  = false;
+
+    private String cId = "";
+    protected CameraDevice cameraDevice;
+    protected CameraCaptureSession session;
+    protected ImageReader imageReader;
+    private static final int REQUEST_CODE_CAMERA = 999;
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private void processImage(Image image){
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    class scb extends CameraDevice.StateCallback {
+        @Override
+        public void onOpened(@NonNull CameraDevice camera) {
+            Log.d(TAG, "CameraDevice.StateCallback onOpened");
+            cameraDevice = camera;
+            actOnReadyCameraDevice();
+        }
+
+        @Override
+        public void onDisconnected(@NonNull CameraDevice camera) {
+            Log.w(TAG, "CameraDevice.StateCallback onDisconnected");
+        }
+
+        @Override
+        public void onError(@NonNull CameraDevice camera, int error) {
+            Log.e(TAG, "CameraDevice.StateCallback onError " + error);
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    class ssc extends CameraCaptureSession.StateCallback {
+
+        @Override
+        public void onReady(CameraCaptureSession session) {
+            MainService.this.session = session;
+            try {
+                session.setRepeatingRequest(createCaptureRequest(), null, null);
+            } catch (CameraAccessException e) {
+                Log.e(TAG, e.getMessage());
+            }
+        }
+
+        @Override
+        public void onConfigured(CameraCaptureSession session) {
+        }
+
+        @Override
+        public void onConfigureFailed(@NonNull CameraCaptureSession session) {
+        }
+    };
+
+    @TargetApi(Build.VERSION_CODES.M)
+     class ial implements ImageReader.OnImageAvailableListener {
+        @Override
+        public void onImageAvailable(ImageReader reader) {
+            Image img = reader.acquireLatestImage();
+            Bitmap bitmap = null;
+            if (img != null) {
+                Image.Plane[] p = img.getPlanes();
+                if (p[0].getBuffer() == null) {
+                    return;
+                }
+                int width = img.getWidth();
+                int height = img.getHeight();
+                int pixelStride = p[0].getPixelStride();
+                int rowStride = p[0].getRowStride();
+                int rowPadding = rowStride - pixelStride * width;
+                byte[] newData = new byte[width * height * 4];
+
+                int offset = 0;
+                Buffer buffer = p[0].getBuffer();
+                buffer.rewind();
+                bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+                //bitmap.copyPixelsFromBuffer(buffer);
+
+                processImage(img);
+                img.close();
+            }
+        }
+    };
+
+    protected CameraDevice.StateCallback cameraStateCallback = new scb();
+    protected CameraCaptureSession.StateCallback sessionStateCallback = new ssc();
+    protected ImageReader.OnImageAvailableListener onImageAvailableListener = new ial();
+
+    @TargetApi(Build.VERSION_CODES.M)
+    public void readyCamera() {
+        CameraManager manager = (CameraManager) getSystemService(CAMERA_SERVICE);
+        try {
+            String pickedCamera = getCamera(manager);
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(getApplicationContext(), "ready permission denied", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            else {
+                manager.openCamera(pickedCamera, cameraStateCallback, null);
+
+                Toast.makeText(getApplicationContext(), "service now start", Toast.LENGTH_SHORT).show();
+                imageReader = ImageReader.newInstance(1920, 1088, ImageFormat.JPEG, 2 /* images buffered */);
+                imageReader.setOnImageAvailableListener(onImageAvailableListener, null);
+            }
+        } catch (CameraAccessException e){
+            Log.e("READY exception", e.getMessage());
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    public String getCamera(CameraManager manager){
+        try {
+            for (String cameraId : manager.getCameraIdList()) {
+                CameraCharacteristics characteristics = manager.getCameraCharacteristics(cameraId);
+                int cOrientation = characteristics.get(CameraCharacteristics.LENS_FACING);
+                if (cOrientation != camchoice) {
+                    return cameraId;
+                }
+            }
+        } catch (CameraAccessException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    public void actOnReadyCameraDevice()
+    {
+        try {
+            cameraDevice.createCaptureSession(Arrays.asList(imageReader.getSurface()), sessionStateCallback, null);
+        } catch (CameraAccessException e){
+            Log.e(TAG, e.getMessage());
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    protected CaptureRequest createCaptureRequest() {
+        try {
+            CaptureRequest.Builder builder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_RECORD);
+            builder.addTarget(imageReader.getSurface());
+            return builder.build();
+        } catch (CameraAccessException e) {
+            Log.e(TAG, e.getMessage());
+            return null;
+        }
+    }
+
 
     @Override
     public IBinder onBind(Intent intent) {
         return null;
     }
+
+    @TargetApi(Build.VERSION_CODES.M)
     @Override
     public void onCreate() {
         super.onCreate();
         isRunning = true;
+        camchoice = CameraCharacteristics.LENS_FACING_BACK;
     }
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d("Start", "onStartCommand");
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-
-                while(isRunning) {
-                    Log.d(TAG,String.valueOf(count));
-                    count++;
-                    try {
-                        Thread.sleep(1000);
-                    } catch (Exception e) {
-                    }
-                }
-
-                stopSelf();
-            }
-        }).start();
+        readyCamera();
         return super.onStartCommand(intent, flags, startId);
     }
 
@@ -49,8 +212,7 @@ public class MainService extends Service {
     public void onDestroy() {
         super.onDestroy();
         isRunning = false;
-
+        Toast.makeText(getApplicationContext(), "service now destroyed", Toast.LENGTH_SHORT).show();
         Log.d("Destroy", "onDestroy");
     }
-
 }
